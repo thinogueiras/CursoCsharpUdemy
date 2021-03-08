1631887174571741d1c45d9ddda01bb3012eade0
Thiago Nogueira dos Santos <thiagonogueira@hotmail.com.br>
2021-03-08 11:52:31 -0300

    Backup alterações e realizado novo exercício de fixação

---
+++ 10.09_Generics.Exercise02/Entities/CountVotes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics.Exercise02.Entities
+{
+    public class CountVotes
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }        
+
+        public CountVotes(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CountVotes))
+            {
+                return false;
+            }
+
+            var countVotesOther = obj as CountVotes;
+            return Name.Equals(countVotesOther.Name);
+        }                
+    }
+}
--- 10.09_Generics.Exercise02/Program.cs
+++ 10.09_Generics.Exercise02/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Generics.Exercise02.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Generics.Exercise02
 {
@@ -6,7 +9,57 @@ namespace Generics.Exercise02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            try
+            {
+                Console.Write("Enter file full path: ");
+                var path = Console.ReadLine();                
+
+                using (var fs = new FileStream(path, FileMode.Open))
+                using (var sr = new StreamReader(fs))
+                {
+                    var dictionary = new Dictionary<string, int>();                    
+
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine().Split(',');
+                        var name = line[0];
+                        var quantity = int.Parse(line[1]);
+                        var countVotes = new CountVotes(name, quantity);                                 
+
+                        if (dictionary.ContainsKey(countVotes.Name))
+                        {
+                            dictionary[countVotes.Name] += quantity;
+                        }
+                        else
+                        {
+                            dictionary[countVotes.Name] = quantity;
+                        }
+                    }
+
+                    Console.WriteLine();
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine($"{item.Key}: {item.Value}");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch (FileLoadException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }       
     }
 }
